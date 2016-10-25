2ce643a4ffacf172721ec420164e117f8c7a3988
xuf <897445793@qq.com>
2016-10-25 18:31:13 +0800

    delete useless import

--- library/src/main/java/com/fangxu/allangleexpandablebutton/AllAngleExpandableButton.java
+++ library/src/main/java/com/fangxu/allangleexpandablebutton/AllAngleExpandableButton.java
@@ -21,7 +21,6 @@ import android.graphics.drawable.Drawable;
 import android.support.annotation.IntDef;
 import android.support.v4.graphics.ColorUtils;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,7 +29,6 @@ import android.view.animation.AnticipateInterpolator;
 import android.view.animation.Interpolator;
 import android.view.animation.OvershootInterpolator;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
